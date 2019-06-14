038891875cde9d0c70d7be73d98ba28a81e2b69d
Joshua Evans <TheJoshuaEvans@gmail.com>
2019-06-14 12:13:41 -0700

    Removed Bad Log

--- local-testing/local-test.js
+++ local-testing/local-test.js
@@ -7,14 +7,17 @@ const ZohoDeskSDK = require('../index.js');
 
   const sdk = new ZohoDeskSDK(opts);
 
-  // const oneContact = await sdk.api.contacts.id('118052000008007001').get();
+  const oneContact = await sdk.api.contacts.id('118052000008007001').get();
   // const contacts = await sdk.api.contacts.get(null, true);
-  const newContact = await sdk.api.contacts.id('118052000008007001').patch({
-    data: {
-      firstName: 'MCTESTERSON'
-    }
-  });
-  console.log(newContact);
-  // console.log(oneContact);
+  // const newContact = await sdk.api.contacts.id('118052000008007001').patch({
+  //   data: {
+  //     firstName: 'MCTESTERSON',
+  //     customFields: {
+  //       Accounts: 'TFN;Pyramid'
+  //     }
+  //   }
+  // });
+  // console.log(newContact);
+  console.log(oneContact);
   // contacts.forEach((item) => console.log(item.email));
 })();
